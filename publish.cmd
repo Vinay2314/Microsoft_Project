18f3038c4544dbc7b21ee1f5e0ed423f4543a388
Vinay2314 <93612728+Vinay2314@users.noreply.github.com>
2022-03-05 00:27:31 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnabotoncovid-bot-Web-Deploy.pubxml -p:Password=pKT2Xgu1LTniz9J1XJAyipYQkqB1Bl3sCm1XJ5SPz3AR49jF0lvoCSaEMGBT
+
